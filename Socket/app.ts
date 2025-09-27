18d2da8798e6487256a3996a840ad38dacad51fb
duc19092005 <duc19092005d@gmail.com>
2025-09-27 20:00:07 +0700

    #4 - Tạo Websocket Server Backend có note lại những thứ cần và chuẩn bị làm

--- Socket/app.ts
+++ Socket/app.ts
@@ -1,20 +1,59 @@
-import express from "express"
-import { Request , Response } from "express";
+import ws from "ws"
+const wssConnection = new ws.Server({port : 8080})
+const userConnection = new Map();
 
-const port = 5500;
-const app = express()
+// Lắng nghe ở port 8080
 
-app.get('/' , (req : Request , res : Response) => {
-    res.status(200)
-})
+wssConnection.on("connection" , function(ws : any, req : any) 
+{
+    console.log(req.url)
+    const url = new URL(req.url , `http://${req.headers.host}`)
+    // Lấy thông tin UserID , Hiện tại chưa có bước xác thực JWT
+    const userId = url.searchParams.get('userId'); 
 
-app.listen(port , (error : any) => {
-    if(!error)
-        console.log("Server is Successfully Running and App is listening on port "+ port);
-    else 
-        console.log("Error occurred, server can't start", error);
+    if(userId)
+    {
+        userConnection.set(userId , ws)
+    }else{
+        ws.close(1008, 'Missing user ID for identification.'); 
+        return;
+    }
+     ws.on('message', function incoming(message : string) {
+        try {
+            const data = JSON.parse(message);
+            
+            if (data.to_user_id && data.content) {
+                
+                const targetWs = userConnection.get(data.to_user_id);
+                
+                if (targetWs && targetWs.readyState === WebSocket.OPEN) {
+                    
+                    const outgoingMessage = JSON.stringify({
+                        type: 'INCOMING_MESSAGE',
+                        from_user_id: userId, 
+                        content: data.content
+                    });
+                    
+                    targetWs.send(outgoingMessage);
+                    console.log(`➡️ Sent message from ${userId} to ${data.to_user_id}`);
+                } else {
+                    // Đây là trong trường hợp nếu Connection Của người nhận Offline thì sẽ tiến hành 
+                    // Lưu trong Database , DB là mongoDB
+                }
+            }
+        } catch (e : any) {
+            console.error('Error processing message:', e.message);
+        }
     });
 
+    // Nếu Client Đóng kết nối thì sẽ tiến hành ngắt kết nối và xóa map
 
-
-
+    ws.on('close', function close() {
+        if (userId) {
+            // Tiến hành ngắt kết nối của Client
+            userConnection.delete(userId);
+            // Sau khi ngắt kết nối sẽ có Logic bộ đếm nó sẽ lưu trong bộ nhớ cached của Server
+            // Nếu người dùng Connect lại thì sẽ ngắt biến đếm và sẽ set trạng thái isOnline = true
+        }
+    });
+})
